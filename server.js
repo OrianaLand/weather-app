f0376f46d0162e801976747600ec90e3cc8b7276
Oriana <98231498+OrianaLand@users.noreply.github.com>
2025-11-10 22:20:58 -0300

    fix: correct wildcard route pattern for Express 5

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ app.get("/api/weather", async (req, res) => {
 });
 
 // Serve index.html for all other routes (SPA support)
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
